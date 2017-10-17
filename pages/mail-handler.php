e5ad2a5b76ac2dea5365dc7961e53c3b1e287e4c
Lauren Mastroni <laurenmastroni@gmail.com>
2017-10-17 18:18:23 +0800

    Update emails

--- pages/mail-handler.php
+++ pages/mail-handler.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['submit'])){
-    $to = "laurenmastroni@gmail.com"; // this is your Email address
+    $to = "info@brandedspirits.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
     $company = $_POST['company'];
