dd682632809a0600b9944b1c1b0316a620ed154b
Lauren Mastroni <laurenmastroni@gmail.com>
2017-11-19 17:38:31 +0800

    Add see more button to mobile map and make auto close menu

--- store-locator/bevmo-map.js
+++ store-locator/bevmo-map.js
@@ -1,9 +1,27 @@
 google.maps.event.addDomListener(window, 'load', function() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: new google.maps.LatLng(37.7749, -122.4194),
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
+  // var map = new google.maps.Map(document.getElementById('map'), {
+  //   center: new google.maps.LatLng(37.7749, -122.4194),
+  //   zoom: 12,
+  //   mapTypeId: google.maps.MapTypeId.ROADMAP
+  // });
+
+  var myLatlng1 = new google.maps.LatLng(37.7749, -122.4194);
+
+     var mapOptions = {
+         zoom: 12,
+         center: myLatlng1,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+     };
+     var map = new google.maps.Map(document.getElementById('map'),
+     mapOptions);
+
+  if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(function (position) {
+             initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+             map.setCenter(initialLocation);
+         });
+     }
+   }
 
   var panelDiv = document.getElementById('panel');
 
