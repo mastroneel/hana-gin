491104981c69ba93d45fb54bf9fa99b42b37c604
Lauren Mastroni <laurenmastroni@gmail.com>
2017-11-20 17:42:06 +0800

    Change map back

--- store-locator/panel.js
+++ store-locator/panel.js
@@ -1,11 +1,6 @@
-
-
 google.maps.event.addDomListener(window, 'load', function() {
 
 
-
-
-  //
   // var map;
   //
   // function initialize() {
@@ -26,37 +21,24 @@ google.maps.event.addDomListener(window, 'load', function() {
   //         });
   //     }
   //
-      // var panelDiv = document.getElementById('panel');
-      //
-      // var data = new MedicareDataSource;
-      //
-      // var view = new storeLocator.View(map, data, {
-      //   geolocation: false,
-      //   features: data.getFeatures()
-      // });
-      //
-      // new storeLocator.Panel(panelDiv, {
-      //   view: view
-      // });
+  //     var panelDiv = document.getElementById('panel');
+  //
+  //     var data = new MedicareDataSource;
+  //
+  //     var view = new storeLocator.View(map, data, {
+  //       geolocation: false,
+  //       features: data.getFeatures()
+  //     });
+  //
+  //     new storeLocator.Panel(panelDiv, {
+  //       view: view
+  //     });
   // }
   // initialize();
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// Works, but doesn't default to San Francisco
   (function() {
 
       if(!!navigator.geolocation) {
@@ -83,14 +65,13 @@ google.maps.event.addDomListener(window, 'load', function() {
 
           });
 
-
       } else {
+
         var map = new google.maps.Map(document.getElementById('map-canvas'), {
           center: new google.maps.LatLng(37.7749, -122.4194),
           zoom: 12,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         });
-
       }
 
       var panelDiv = document.getElementById('panel');
