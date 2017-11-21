bd634c147ba60f597be9fc6b593806d4706e09b5
Lauren Mastroni <laurenmastroni@gmail.com>
2017-11-21 16:54:15 +0800

    remove commented out code

--- store-locator/panel.js
+++ store-locator/panel.js
@@ -1,44 +1,4 @@
 google.maps.event.addDomListener(window, 'load', function() {
-
-
-  // var map;
-  //
-  // function initialize() {
-  //     var myLatlng1 = new google.maps.LatLng(37.7749, -122.4194);
-  //
-  //     var mapOptions = {
-  //         zoom: 12,
-  //         center: myLatlng1,
-  //         mapTypeId: google.maps.MapTypeId.ROADMAP
-  //     };
-  //     var map = new google.maps.Map(document.getElementById('map-canvas'),
-  //     mapOptions);
-  //
-  //     if (navigator.geolocation) {
-  //         navigator.geolocation.getCurrentPosition(function (position) {
-  //             initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  //             map.setCenter(initialLocation);
-  //         });
-  //     }
-  //
-  //     var panelDiv = document.getElementById('panel');
-  //
-  //     var data = new MedicareDataSource;
-  //
-  //     var view = new storeLocator.View(map, data, {
-  //       geolocation: false,
-  //       features: data.getFeatures()
-  //     });
-  //
-  //     new storeLocator.Panel(panelDiv, {
-  //       view: view
-  //     });
-  // }
-  // initialize();
-
-
-
-
   (function() {
     function error(err) {
       console.log('ERROR('+err.code+'): '+err.message);
@@ -59,11 +19,6 @@ google.maps.event.addDomListener(window, 'load', function() {
           navigator.geolocation.getCurrentPosition(function(position) {
 
               var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-              // var infowindow = new google.maps.InfoWindow({
-              //     map: map,
-              //     position: geolocate,
-              // });
 
               map.setCenter(geolocate);
 
@@ -98,38 +53,4 @@ google.maps.event.addDomListener(window, 'load', function() {
       });
 
   })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // var map = new google.maps.Map(document.getElementById('map-canvas'), {
-  //   center: new google.maps.LatLng(37.7749, -122.4194),
-  //   zoom: 12,
-  //   mapTypeId: google.maps.MapTypeId.ROADMAP
-  // });
-
-  // var panelDiv = document.getElementById('panel');
-  //
-  // var data = new MedicareDataSource;
-  //
-  // var view = new storeLocator.View(map, data, {
-  //   geolocation: false,
-  //   features: data.getFeatures()
-  // });
-  //
-  // new storeLocator.Panel(panelDiv, {
-  //   view: view
-  // });
 });
