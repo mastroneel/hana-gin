10beb2a9ab52b7797486931e9556b16c85a75f83
Lauren Mastroni <laurenmastroni@gmail.com>
2017-12-11 12:55:40 +0800

    Add scroll to top button

--- php/mail_handler.php
+++ php/mail_handler.php
@@ -8,14 +8,14 @@ if(isset($_POST['submit'])){
     $phone = $_POST['phone'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+    $message = $name . " wrote the following:" . "\n\n" . "Name: " . $name . "\n\n" . "Company name: " . $company . "\n\n" .  "Company Type: " . $companytype . "\n\n" . "From: " . $from . "\n\n" . "Phone: " . $phone . "\n\n" . "Message: " . $_POST['message'];
+    $message2 = "Here is a copy of your message " . "\n\n" . "Name: " . $name . "\n\n" . "Company name: " . $company . "\n\n" .  "Company Type: " . $companytype . "\n\n" . "From: " . $from . "\n\n" . "Phone: " . $phone . "\n\n" . "Message: " . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
     }
